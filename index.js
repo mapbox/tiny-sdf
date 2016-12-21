f0a158f3673178f024a0baba8ce9f023c9882bc8
Vladimir Agafonkin <agafonkin@gmail.com>
2016-12-21 14:35:43 +0200

    try more precise antialiasing processing

--- index.js
+++ index.js
@@ -39,16 +39,12 @@ TinySDF.prototype.draw = function (char) {
     var imgData = this.ctx.getImageData(0, 0, this.size, this.size);
     var data = imgData.data;
 
-    for (var i = 0; i < this.size * this.size; i++) {
-        var a = data[i * 4 + 3] / 255; // alpha value
-        this.gridOuter[i] = a === 1 ? 0 : a === 0 ? INF : Math.pow(Math.max(0, 0.5 - a), 2);
-        this.gridInner[i] = a === 1 ? INF : a === 0 ? 0 : Math.pow(Math.max(0, a - 0.5), 2);
-    }
+    fillEdgeDistances(imgData.data, this.gridOuter, this.gridInner, this.size);
 
     edt(this.gridOuter, this.size, this.size, this.f, this.d, this.v, this.z);
     edt(this.gridInner, this.size, this.size, this.f, this.d, this.v, this.z);
 
-    for (i = 0; i < this.size * this.size; i++) {
+    for (var i = 0; i < this.size * this.size; i++) {
         var d = this.gridOuter[i] - this.gridInner[i];
         var c = Math.max(0, Math.min(255, Math.round(255 - 255 * (d / this.radius + this.cutoff))));
         data[4 * i + 0] = c;
@@ -59,6 +55,50 @@ TinySDF.prototype.draw = function (char) {
 
     return imgData;
 };
+
+var SQRT2 = Math.sqrt(2);
+
+function fillEdgeDistances(img, gridOuter, gridInner, n) {
+    for (var i = 0; i < n * n; i++) {
+        var a = img[i * 4 + 3] / 255; // alpha value
+        var d = 0.5 - a;
+        if (a > 0 && a < 1) {
+            var tr = img[(i - n + 1) * 4 + 3] / 255;
+            var br = img[(i + n + 1) * 4 + 3] / 255;
+            var tl = img[(i - n - 1) * 4 + 3] / 255;
+            var bl = img[(i + n - 1) * 4 + 3] / 255;
+            var l = img[(i - 1) * 4 + 3] / 255;
+            var r = img[(i + 1) * 4 + 3] / 255;
+            var t = img[(i - n) * 4 + 3] / 255;
+            var b = img[(i + n) * 4 + 3] / 255;
+            var gx = Math.abs(tr + SQRT2 * r + br - tl - SQRT2 * l - bl);
+            var gy = Math.abs(bl + SQRT2 * b + br - tl - SQRT2 * t - tr);
+
+            if (gx > 0 && gy > 0) {
+                var glen = Math.sqrt(gx * gx + gy * gy);
+                gx /= glen;
+                gy /= glen;
+
+                if (gx < gy) {
+                    var temp = gx;
+                    gx = gy;
+                    gy = temp;
+                }
+
+                var a1 = 0.5 * gy / gx;
+                if (a < a1) { // 0 <= a < a1
+                    d = 0.5 * (gx + gy) - Math.sqrt(2.0 * gx * gy * a);
+                } else if (a < (1.0 - a1)) { // a1 <= a <= 1-a1
+                    d = (0.5 - a) * gx;
+                } else { // 1-a1 < a <= 1
+                    d = -0.5 * (gx + gy) + Math.sqrt(2.0 * gx * gy * (1.0 - a));
+                }
+            }
+        }
+        gridOuter[i] = a === 1 ? 0 : a === 0 ? INF : Math.pow(Math.max(0, d), 2);
+        gridInner[i] = a === 1 ? INF : a === 0 ? 0 : Math.pow(Math.max(0, -d), 2);
+    }
+}
 
 // 2D Euclidean distance transform by Felzenszwalb & Huttenlocher https://cs.brown.edu/~pff/dt/
 function edt(data, width, height, f, d, v, z) {
