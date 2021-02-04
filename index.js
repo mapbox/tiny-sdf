82222b65d0c363199ff709325aa522a89a9687f7
James Seppi <james.seppi@mapbox.com>
2021-02-04 16:58:07 -0700

    golf some tiny improvements

--- index.js
+++ index.js
@@ -157,17 +157,18 @@ function edt(data, width, height, f, v, z) {
 
 // 1D squared distance transform
 function edt1d(grid, offset, stride, length, f, v, z) {
-    var q, k, s, r;
+    var q, k, s, r, q_sq;
     v[0] = 0;
     z[0] = -INF;
     z[1] = INF;
-
-    for (q = 0; q < length; q++) f[q] = grid[offset + q * stride];
+    f[0] = grid[offset];
 
     for (q = 1, k = 0, s = 0; q < length; q++) {
+        f[q] = grid[offset + q * stride];
+        q_sq = q * q;
         do {
             r = v[k];
-            s = (f[q] - f[r] + q * q - r * r) / (q - r) / 2;
+            s = (f[q] - f[r] + q_sq - r * r) / (q - r) / 2;
         } while (s <= z[k] && --k > -1);
 
         k++;
