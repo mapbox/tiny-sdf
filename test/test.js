e7bd4da66b6b028b8c00849e30eb09218e8aa51c
Vladimir Agafonkin <agafonkin@gmail.com>
2021-02-21 22:19:38 +0200

    mock fill/measureText for deterministic tests

--- test/test.js
+++ test/test.js
@@ -6,23 +6,53 @@ import {readFileSync, writeFileSync} from 'fs';
 import {test} from 'tape';
 import pixelmatch from 'pixelmatch';
 
-const fixtureA = PNG.sync.read(readFileSync(new URL('./glyph.png', import.meta.url)));
-
-class NodeTinySDF extends TinySDF {
-    _createCanvas(size) {
-        return nodeCanvas.createCanvas(size, size);
-    }
-}
+const baseUrl = import.meta.url;
 
 test('draws an SDF given a character', (t) => {
-    const sdf = new NodeTinySDF({
-        fontSize: 48
+
+    class MockTinySDF extends TinySDF {
+        _createCanvas(size) {
+            return nodeCanvas.createCanvas(size, size);
+        }
+    }
+
+    const rawUrl = new URL('./fixtures/1-raw.png', baseUrl);
+    const sdfUrl = new URL('./fixtures/1-sdf.png', baseUrl);
+    const metricsUrl = new URL('./fixtures/1-metrics.json', baseUrl);
+
+    const sdf = new MockTinySDF({
+        fontSize: 48,
+        buffer: 3
     });
+
+    const originalMeasureText = sdf.ctx.measureText;
+
+    sdf.ctx.measureText = function (text) {
+        if (process.env.UPDATE) {
+            const metrics = originalMeasureText.call(this, text);
+            writeFileSync(metricsUrl, JSON.stringify(metrics, null, 2));
+        }
+        return JSON.parse(readFileSync(metricsUrl));
+    };
+
+    const originalFillText = sdf.ctx.fillText;
+
+    sdf.ctx.fillText = function (text, x, y) {
+        if (process.env.UPDATE) {
+            originalFillText.call(this, text, x, y);
+            const {width, height} = this.canvas;
+            const png = new PNG({width, height});
+            png.data.set(this.getImageData(0, 0, width, height).data);
+            writeFileSync(rawUrl, PNG.sync.write(png));
+        }
+        const png = PNG.sync.read(readFileSync(rawUrl));
+        this.putImageData(nodeCanvas.createImageData(new Uint8ClampedArray(png.data.buffer), png.width), 0, 0);
+    };
 
     const {data, width, height, glyphWidth, glyphHeight, glyphTop, glyphLeft, glyphAdvance} = sdf.draw('材');
 
-    t.equal(width, fixtureA.width, 'width');
-    t.equal(height, fixtureA.height, 'height');
+    t.equal(width, 54, 'width');
+    t.equal(height, 50, 'height');
     t.equal(glyphWidth, 48, 'glyphWidth');
     t.equal(glyphHeight, 44, 'glyphHeight');
     t.equal(glyphTop, 39, 'glyphTop');
@@ -41,10 +71,11 @@ test('draws an SDF given a character', (t) => {
     if (process.env.UPDATE) {
         const png = new PNG({width, height});
         png.data.set(actualImg);
-        writeFileSync(new URL('./glyph.png', import.meta.url), PNG.sync.write(png));
+        writeFileSync(sdfUrl, PNG.sync.write(png));
     }
 
-    const numDiffPixels = pixelmatch(fixtureA.data, actualImg, undefined, width, height, {threshold: 0, includeAA: true});
+    const expectedImg = PNG.sync.read(readFileSync(sdfUrl));
+    const numDiffPixels = pixelmatch(expectedImg.data, actualImg, undefined, width, height, {threshold: 0, includeAA: true});
 
     t.equal(numDiffPixels, 0, 'SDF pixels');
 
