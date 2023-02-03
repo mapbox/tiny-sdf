6f2f77e6ed8ef056979c921687a0ed48529b6576
Vladimir Agafonkin <agafonkin@gmail.com>
2023-02-03 18:45:32 +0200

    fixup for travis

--- test/test.js
+++ test/test.js
@@ -92,6 +92,17 @@ test('does not crash on diacritic marks', (t) => {
 
 test('does not return negative-width glylphs', (t) => {
     const sdf = new MockTinySDF();
+    // stub these because they vary across environments
+    sdf.ctx.measureText = () => ({
+        width: 0,
+        actualBoundingBoxLeft: 23.3759765625,
+        actualBoundingBoxRight: -17.6162109375,
+        actualBoundingBoxAscent: 20.2080078125,
+        actualBoundingBoxDescent: -14.51953125,
+        emHeightAscent: 26,
+        emHeightDescent: 9,
+        alphabeticBaseline: 7.51953125
+    });
     const glyph = sdf.draw('゙');
     t.equal(glyph.glyphWidth, 0);
     t.equal(glyph.width, 6); // zero-width glyph with 3px buffer
